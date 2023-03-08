af4590b929f853f5284a8f55eb7695a07d0df551
Marco Rebhan <me@dblsaiko.net>
2023-03-08 13:57:41 +0100

    Don't update chunks when rendering lights
    Due to frustum updates now being asynchronous, the updates started
    while rendering from a light's perspective can "bleed" into the main
    camera view and vice versa, causing chunks to flicker because they
    are not visible from the other perspective.
    
    The updateChunks mixin is just there in case that method could cause
    a similar problem, it should not be necessary to fix this bug.

--- src/main/java/net/dblsaiko/illuminate/mixin/WorldRendererMixin.java
+++ src/main/java/net/dblsaiko/illuminate/mixin/WorldRendererMixin.java
@@ -3,6 +3,7 @@ package net.dblsaiko.illuminate.mixin;
 import net.dblsaiko.illuminate.client.GameRendererExt;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.Camera;
+import net.minecraft.client.render.Frustum;
 import net.minecraft.client.render.WorldRenderer;
 import net.minecraft.entity.Entity;
 import org.spongepowered.asm.mixin.Final;
@@ -11,6 +12,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(WorldRenderer.class)
@@ -35,5 +37,27 @@ public class WorldRendererMixin {
         }
 
         return this.client.player;
+    }
+
+    @Inject(
+        method = "setupTerrain(Lnet/minecraft/client/render/Camera;Lnet/minecraft/client/render/Frustum;ZZ)V",
+        at = @At("HEAD"),
+        cancellable = true
+    )
+    private void setupTerrain(Camera camera, Frustum frustum, boolean hasForcedFrustum, boolean spectator, CallbackInfo ci) {
+        if (GameRendererExt.from(this.client.gameRenderer).activeRenderLight() != null) {
+            ci.cancel();
+        }
+    }
+
+    @Inject(
+        method = "updateChunks(Lnet/minecraft/client/render/Camera;)V",
+        at = @At("HEAD"),
+        cancellable = true
+    )
+    private void updateChunks(Camera camera, CallbackInfo ci) {
+        if (GameRendererExt.from(this.client.gameRenderer).activeRenderLight() != null) {
+            ci.cancel();
+        }
     }
 }
